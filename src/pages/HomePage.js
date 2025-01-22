45fb52444bd039ec663c854a131b4e22adcc2fbe
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-01-22 10:45:40 -0500

    Welcome Page Created

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import "../styles/HomePage.css";
+import welcomeImage from "../assets/WelcomeImage.jpg"; 
 const HomePage = () => {
-  return <h1>Welcome to Friendship Plus</h1>;
+  const handleLoginClick = () => {
+    window.location.href = "/signup"; 
+  };
+
+  const handleSignupClick = () => {
+    window.location.href = "/signup"; 
+  };
+
+  return (
+    <div className="homepage-container">
+      <main className="main-content">
+        <h1>Friendship Plus</h1>
+        <div className="button-group">
+          <button className="action-button" onClick={handleLoginClick}>Log In</button>
+          <button className="action-button" onClick={handleSignupClick}>Sign up Now!</button>
+        </div>
+        <div>
+          <img src={welcomeImage} alt="Welcome" className="welcome-image" />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default HomePage;
