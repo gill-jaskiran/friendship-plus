0c4a1ab3dd4a1608802d7c8da900f68550e49a13
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-01-22 20:37:56 -0500

    CSS updated

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React from "react";
+import "../styles/AboutPage.css";
+import infoIcon from "../assets/InfoIcon.png"; 
 
 const AboutPage = () => {
-    return (
-        <div className="about-container">
-          <h1>About Friendship Plus</h1>
-          <p>We believe in helping students create long-lasting friendships in an educational setting.</p>
+  return (
+    <div className="about-container">
+      <div className="about-header">
+        <img src={infoIcon} alt="Info Icon" className="about-icon" />
+        <h1 className="about-title">About</h1>
+      </div>
+
+      <div className="content-section">
+        <div className="content-left">WHAT WE BELIEVE</div>
+        <div className="content-right">
           <p>
-            Friendship Plus is intended for post-secondary students who find it difficult to form lasting friendships when entering a new environment.
+            We believe in helping students create long-lasting friendships in an educational setting.
           </p>
           <p>
-            Friendship Plus is a student-based social media networking platform that allows students to connect with like-minded individuals, discover clubs of interests, form groups within classes, and network.
+            Friendship Plus is intended for post-secondary students who find it
+            difficult to form lasting friendships when entering a new environment.
+            Friendship Plus is a student-based social media networking platform that
+            allows students to connect with like-minded individuals, discover clubs
+            of interest, form groups within classes, and network.
           </p>
         </div>
-      );
-    };
-
+      </div>
+    </div>
+  );
+};
 
 export default AboutPage;
+
+
